b61f25d9e2b52bab3650c527313918c7f52b17ec
MNewbigging <mrnewbigging@gmail.com>
2021-09-04 14:28:57 +0100

    blank slate for redesign

--- src/Blether.tsx
+++ src/Blether.tsx
@@ -1,62 +1,7 @@
-import { observer } from 'mobx-react';
 import React from 'react';
 
-import { BletherScreen, BletherState, BletherViewMode } from './BletherState';
-import { BLChatDesktop } from './screens/chat/desktop/BLChatDesktop';
-import { BLChatMobile } from './screens/chat/mobile/BLChatMobile';
-import { BLHomeDialog } from './screens/home/BLHomeDialog';
-
-import './blether.scss';
-import { BLChatState } from './screens/chat/BLChatState';
-
-@observer
-export class Blether extends React.PureComponent {
-  private readonly bState = new BletherState();
-
-  componentDidMount() {
-    window.onresize = this.onResizeWindow;
-  }
-
-  componentWillUnmount() {
-    window.onresize = undefined;
-  }
-
+export class Blether extends React.Component {
   public render() {
-    let screen: JSX.Element;
-
-    if (this.bState.bScreen === BletherScreen.HOME || !this.bState.participant?.ready) {
-      screen = <BLHomeDialog bState={this.bState} />;
-    } else {
-      if (this.bState.chatState === undefined && this.bState.participant) {
-        this.bState.chatState = new BLChatState(this.bState.participant);
-      }
-
-      switch (this.bState.viewMode) {
-        case BletherViewMode.DESKTOP:
-          screen = (
-            <BLChatDesktop
-              participant={this.bState.participant}
-              chatState={this.bState.chatState}
-              onExit={() => this.bState.exitChat()}
-            />
-          );
-          break;
-        case BletherViewMode.MOBILE:
-          screen = (
-            <BLChatMobile
-              participant={this.bState.participant}
-              chatState={this.bState.chatState}
-              onExit={() => this.bState.exitChat()}
-            />
-          );
-          break;
-      }
-    }
-
-    return <div className={'blether-app'}>{screen}</div>;
+    return <div>blether</div>;
   }
-
-  private readonly onResizeWindow = () => {
-    this.bState.checkViewMode(window.innerWidth);
-  };
 }
